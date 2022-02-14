ca33f8c55a671d88219e17ddc2f6833e31358abe
Fakhri-P127 <fakhrita@code.edu.az>
2022-02-14 20:50:32 +0400

    added Task-1(main) folder

---
+++ Task-1(main)/Task-1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {                        
+            SumofScores();            
+        }
+        public static int SumofScores( )
+        {
+            int[] studentScores = { 60, 65, 50, 25, 90, 100, 100, 30, 0, 45, 80 };
+            int numofStudents=studentScores.Length;             
+            int sum = 0;
+            
+            for (int i = 0; i < studentScores.Length; i++)
+            {
+                sum += studentScores[i];
+            }                        
+            AvgScore(ref numofStudents, ref sum);
+            return sum;                       
+        }
+        public static int AvgScore(ref int numofStudents, ref int sum )
+        {
+            int avgOfClass = sum / numofStudents ;
+            Console.WriteLine("Sinifin ortalama bali: "+ avgOfClass);
+            return avgOfClass;
+        }
+    }
+}
